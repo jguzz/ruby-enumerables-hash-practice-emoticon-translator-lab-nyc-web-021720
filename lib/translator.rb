e963de4b0f7bf4d596a0de7fd5cc1c14ca7b805d
Joseph Guzzardo <joseph.a.guzzardo@gmail.com>
2020-02-14 19:27:53 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,7 +14,7 @@ end
 
 def get_japanese_emoticon(file_path, emoticon)
   # code goes here
-  chip = ""
+  x = ""
   library = load_library(file_path)
   library.each do |type, emotes|
     emotes.each do |library_emoticon|
