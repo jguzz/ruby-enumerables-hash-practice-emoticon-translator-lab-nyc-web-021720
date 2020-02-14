d7a90c442b5d945252383e260e0ad84119f0a5db
Joseph Guzzardo <joseph.a.guzzardo@gmail.com>
2020-02-14 19:34:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,7 +21,7 @@ def get_japanese_emoticon(file_path, english_emoticon)
   else
     japanese_emoticon = "Sorry, that emoticon was not found"
   end
-  x
+  japanese_emoticon
 end
 
 def get_english_meaning
