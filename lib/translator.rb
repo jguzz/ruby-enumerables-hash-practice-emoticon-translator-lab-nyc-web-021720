a601eae91aa60925adbf8656d5d4fc8584daab9d
Joseph Guzzardo <joseph.a.guzzardo@gmail.com>
2020-02-14 19:25:45 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,11 +14,11 @@ end
 
 def get_japanese_emoticon(file_path, emoticon)
   # code goes here
-  library = YAML.load_file(file_path)
+  library = load_library(file_path)
   library.each do |type, emotes|
     emotes.each do |library_emoticon|
       if emoticon == library_emoticon
-        
+
       end
     end
   end
