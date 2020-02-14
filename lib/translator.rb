4d9cb9f655b89f32b7720f6aa7f2a2d5b6a1084b
Joseph Guzzardo <joseph.a.guzzardo@gmail.com>
2020-02-14 19:10:37 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,7 +6,7 @@ def load_library(file_path)
   emoticons_yml = YAML.load_file(file_path)
   emot = {"get_meaning" => {}, "get_emoticon" => {}}
   emoticons_yml.each do |type, emote|
-    emot["get_meaning"][emote[1]] = type[0]
+    emot["get_meaning"][emote[1]] = type
     emot["get_emoticon"][emote[0]] = emote[1]
   end
   emot
